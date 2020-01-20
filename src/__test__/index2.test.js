dc7594adf9e7cf309f135d76143c46bb26dd1f6d
Lise Bilhaut <lbilhaut@niche.com>
2020-01-20 16:23:07 -0500

    [jest-convert] ran jest-codemods

--- src/__test__/index2.test.js
+++ src/__test__/index2.test.js
@@ -1,75 +1,74 @@
-import { expect } from 'chai'
 import sinon from 'sinon'
 import { add, sub, mul, div, isString, prop, once } from './index'
 
 
 describe('Testing assorted utils', () => {
-  context('add', () => {
-    it('Adds two numbers', () => {
+  describe('add', () => {
+    test('Adds two numbers', () => {
       const result = add(1, 2)
-      expect(result).to.equal(3)
+      expect(result).toBe(3)
     })
   })
 
-  context('sub', () => {
-    it('Subtracts one number from another', () => {
+  describe('sub', () => {
+    test('Subtracts one number from another', () => {
       const result = sub(3, 1)
-      expect(result).to.equal(2)
+      expect(result).toBe(2)
     })
   })
 
-  context('mul', () => {
-    it('Multiplies numbers', () => {
+  describe('mul', () => {
+    test('Multiplies numbers', () => {
       const result = mul(3, 2)
-      expect(result).to.equal(6)
+      expect(result).toBe(6)
     })
   })
 
-  context('div', () => {
-    it('Divides first number by the second', () => {
+  describe('div', () => {
+    test('Divides first number by the second', () => {
       const result = div(6, 2)
-      expect(result).to.eq(3)
+      expect(result).toBe(3)
     })
   })
 
-  context('isString', () => {
-    it('Returns true for a string', () => {
+  describe('isString', () => {
+    test('Returns true for a string', () => {
       const input = 'Hello world'
       const result = isString(input)
-      expect(result).to.be.true
+      expect(result).toBe(true)
     })
 
-    it('Returns false for a number', () => {
+    test('Returns false for a number', () => {
       const input = 5
       const result = isString(input)
-      expect(result).to.be.false
+      expect(result).toBe(false)
     })
 
-    it('Returns true for an interpolated string', () => {
+    test('Returns true for an interpolated string', () => {
       const number = 127
       const input = `The number is ${number}`
       const result = isString(input)
-      expect(result).to.be.true
+      expect(result).toBe(true)
     })
   })
 
-  context('prop', () => {
-    it('Returns the value for an existing prop', () => {
+  describe('prop', () => {
+    test('Returns the value for an existing prop', () => {
       const input = { a: 'A', b: 'B' }
       const result = prop('a', input)
-      expect(result).not.to.be.undefined
-      expect(result).to.equal('A')
+      expect(result).not.toBeUndefined()
+      expect(result).toBe('A')
     })
 
-    it('Returns undefined for a non-existent prop', () => {
+    test('Returns undefined for a non-existent prop', () => {
       const input = { a: 'A', b: 'B' }
       const result = prop('x', input)
-      expect(result).to.be.undefined
+      expect(result).toBeUndefined()
     })
   })
 
-  context('once', () => {
-    it('Only invokes the function once', () => {
+  describe('once', () => {
+    test('Only invokes the function once', () => {
       const fn = sinon.stub()
       const onceFn = once(fn)
       onceFn()
